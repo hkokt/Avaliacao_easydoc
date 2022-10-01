b8b53a1ee0814077f08a60284fc7c6154cbc7493
Hugo Nogueira Koketu <96093334+hkokt@users.noreply.github.com>
2022-10-01 19:47:51 -0300

    Update cliente.java

--- src/model/cliente.java
+++ src/model/cliente.java
@@ -5,9 +5,5 @@ public class cliente {
 	public String rg;
 	public String cnh;
 	public String cert;
-	@Override
-	public String toString() {
-		return "cliente [rg = " + rg + ", cnh = " + cnh + ", cert = " + cert + "]";
-	}
 
 }
